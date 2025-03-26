39ceaeb0f3d6ff1363bb40badfbb6cdf78c9857d
delila-m <128874589+delila-m@users.noreply.github.com>
2025-03-26 14:09:39 -0700

    Update USA_Processing.R

--- USA_Processing.R
+++ USA_Processing.R
@@ -115,19 +115,19 @@ rf.caret.fit <- train(USDM_Avg ~ PDSI_Avg + bin.x + bin.y,
                 method = "rf", 
                 ntree = 50,
                 trControl = ctrl)
-# using ranger 
-library(ranger)
-
-rf.ranger.fit <- ranger(
-  formula = USDM_Avg ~ PDSI_Avg + bin.x + bin.y,  # Formula specifying the target and predictors
-  data = train,  # Training dataset
-  num.trees = 100,  # Number of trees in the forest
-  mtry = 2,  # Number of features to consider at each split
-  classification = TRUE,  # Specify that it's a classification problem
-  importance = 'impurity',  # To measure feature importance
-  probability = TRUE,  # To get probabilities for each class
-  verbose = TRUE
-  )
+# using ranger does not work :(
+# library(ranger)
+# 
+# rf.ranger.fit <- ranger(
+#   formula = USDM_Avg ~ PDSI_Avg + bin.x + bin.y,  # Formula specifying the target and predictors
+#   data = train,  # Training dataset
+#   num.trees = 100,  # Number of trees in the forest
+#   mtry = 2,  # Number of features to consider at each split
+#   classification = TRUE,  # Specify that it's a classification problem
+#   importance = 'impurity',  # To measure feature importance
+#   probability = TRUE,  # To get probabilities for each class
+#   verbose = TRUE
+#   )
 
 # predict and find RMSE
 preds <- predict(rf.fit, test)
